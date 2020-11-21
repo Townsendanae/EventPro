72cdebe41510399f088b4b61ace870f8c8a1a885
townsendanae <danjtown@espol.edu.ec>
2020-11-21 12:15:10 -0500

    Creación del paquete papeleo, con las clases ordenPago, Solicitud y todas las enum. Faltan métodos y constructores :)

---
+++ src/Papeleo/OrdenPago.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Papeleo;
+import java.util.*;
+import Usuario.*;
+/**
+ *
+ * @author danae
+ */
+public class OrdenPago {
+    private int idTransaccion;
+    private Date fechaRegistro;
+    private Cliente cliente;
+    //private Evento evento;
+    private EstadoPago estadoPago;
+    private double TotalPagar;
+    
+    
+    
+}
