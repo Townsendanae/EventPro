4649ccc8c53c5aca0e24944680fd4a6ca44a508e
townsendanae <danjtown@espol.edu.ec>
2020-11-21 18:24:44 -0500

    Coloqué un paquete con las clases que la miss dejó para trabajar con archivos

--- src/Papeleo/OrdenPago.java
+++ src/Papeleo/OrdenPago.java
@@ -6,18 +6,114 @@
 package Papeleo;
 import java.util.*;
 import Usuario.*;
+import trabajoconarchivos.*;
 /**
  *
  * @author danae
  */
 public class OrdenPago {
-    private int idTransaccion;
+    private long idTransaccion;
+    private long codigo; // Generado de forma aleatoria
     private Date fechaRegistro;
     private Cliente cliente;
     //private Evento evento;
     private EstadoPago estadoPago;
-    private double TotalPagar;
+    private double totalPagar;
     
+   //Constructor
     
+    public OrdenPago(Cliente cliente){ //Falta evento
+        this.cliente = cliente;
+        this.estadoPago = EstadoPago.PENDIENTEPAGO;
+        this.codigo = obtieneCodigo();
+        //el total a pagar con el objeto evento
+    }
+    
+    //ID Transacción
+    
+    public long getIdTransaccion(){
+        return this.idTransaccion;
+    }
+    
+    public void setIdTransaccion(long idTransaccion){
+        this.idTransaccion = idTransaccion;
+    }
+    
+    // ID orden
+    
+    public long getIdOrden(){
+        return this.codigo;
+    }
+    
+    public void setIdOrden(long codigo){
+        this.codigo = codigo;
+    }
+    
+    //Fecha Registro
+    
+    public Date getFechaRegistro(){
+        return this.fechaRegistro;
+    }
+    
+    //la fecha de registro no se deberá modificar. 
+    
+    //Cliente
+    
+    public Cliente getCliente(){
+        return this.cliente;
+    }
+    
+    public void setCliente(Cliente cliente){
+        this.cliente = cliente;
+    }
+    
+    //Evento
+    
+    //public Evento getEvento(){
+    //    return this.evento;
+    //}
+    
+    //public void setEvento(Evento evento){
+    //    this.evento = evento;
+    //}
+    
+    // Estado Pago
+    
+    public EstadoPago getEstadoPago(){
+        return this.estadoPago;
+    }
+    
+    public void setEstadoPago(EstadoPago estadoPago){
+        this.estadoPago = estadoPago;
+    }
+    
+    //Total Pagar 
+    
+    public double getTotalPagar(){
+        return this.totalPagar;
+    }
+    
+    public void setTotalPagar(double totalPagar){
+        this.totalPagar = totalPagar;
+    }
+    
+   
+    
+    // Métodos: 
+    public void guardarDatosPago(){  
+        String info = this.codigo + " " + "codigo_evento con un get" + " " + this.totalPagar + " " +this.estadoPago + " " + this.idTransaccion + " " + this.fechaRegistro;
+        ManejoArchivos.EscribirArchivo("ordenPago.txt",info);
+    }
+    
+    public void mostrarMensaje(){
+        System.out.println("Código de la orden: "+this.idTransaccion);
+    }
+    
+    //Generar codigo aleatorio
+    
+    private static int obtieneCodigo() {
+       Random r = new Random();
+       return r.nextInt(1000);
+    }
     
 }
---
+++ src/trabajoconarchivos/TrabajoConArchivos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajoconarchivos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Verónica
+ */
+public class TrabajoConArchivos {
+
+  
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] arg) {
+        ManejoArchivos.EscribirArchivo("archivosalida.txt","Hola");
+        ManejoArchivos.EscribirArchivo("archivosalida.txt","Afios!");
+        ManejoArchivos.LeeFichero("archivo.txt");
+
+    }
+    
+}
