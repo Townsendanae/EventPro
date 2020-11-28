49afbf7317adfb547a73edcfe866a8b966616972
townsendanae <danjtown@espol.edu.ec>
2020-11-28 02:21:03 -0500

    Clase Adicional

--- src/Papeleo/OrdenPago.java
+++ src/Papeleo/OrdenPago.java
@@ -7,6 +7,7 @@ package Papeleo;
 import java.util.*;
 import Usuario.*;
 import trabajoconarchivos.*;
+import Eventos.*;
 /**
  *
  * @author danae
@@ -16,90 +17,81 @@ public class OrdenPago {
     private long codigo; // Generado de forma aleatoria
     private Date fechaRegistro;
     private Cliente cliente;
-    //private Evento evento;
+    private Evento evento;
     private EstadoPago estadoPago;
     private double totalPagar;
     
-   //Constructor
+   //----------Constructor------------
     
-    public OrdenPago(Cliente cliente){ //Falta evento
+    public OrdenPago(Cliente cliente, Evento evento){ 
         this.cliente = cliente;
+        this.evento = evento;
         this.estadoPago = EstadoPago.PENDIENTEPAGO;
         this.codigo = obtieneCodigo();
-        //el total a pagar con el objeto evento
+        this.totalPagar = evento.getPrecio();
     }
     
-    //ID Transacción
     
-    public long getIdTransaccion(){
-        return this.idTransaccion;
-    }
+    // ---------Setters ---------------
     
     public void setIdTransaccion(long idTransaccion){
         this.idTransaccion = idTransaccion;
-    }
-    
-    // ID orden
-    
-    public long getIdOrden(){
-        return this.codigo;
     }
     
     public void setIdOrden(long codigo){
         this.codigo = codigo;
     }
     
-    //Fecha Registro
-    
-    public Date getFechaRegistro(){
-        return this.fechaRegistro;
-    }
-    
-    //la fecha de registro no se deberá modificar. 
-    
-    //Cliente
-    
-    public Cliente getCliente(){
-        return this.cliente;
-    }
-    
     public void setCliente(Cliente cliente){
         this.cliente = cliente;
     }
     
-    //Evento
-    
-    //public Evento getEvento(){
-    //    return this.evento;
-    //}
-    
-    //public void setEvento(Evento evento){
-    //    this.evento = evento;
-    //}
-    
-    // Estado Pago
-    
-    public EstadoPago getEstadoPago(){
-        return this.estadoPago;
+    public void setEvento(Evento evento){
+        this.evento = evento;
     }
     
     public void setEstadoPago(EstadoPago estadoPago){
         this.estadoPago = estadoPago;
     }
     
-    //Total Pagar 
-    
-    public double getTotalPagar(){
-        return this.totalPagar;
-    }
-    
     public void setTotalPagar(double totalPagar){
         this.totalPagar = totalPagar;
     }
     
+    // ------- Getters ----------
+    
+    public long getIdTransaccion(){
+        return this.idTransaccion;
+    }
+    
+    public long getIdOrden(){
+        return this.codigo;
+    }
+ 
+    public Date getFechaRegistro(){
+        return this.fechaRegistro;
+    }
+    
+    public Cliente getCliente(){
+        return this.cliente;
+    }
+    
+    public Evento getEvento(){
+        return this.evento;
+    }
+    
+    public EstadoPago getEstadoPago(){
+        return this.estadoPago;
+    }
+    
+    public double getTotalPagar(){
+        return this.totalPagar;
+    }
+      
+    
    
     
-    // Métodos: 
+    // -------------Métodos------------- 
     public void guardarDatosPago(){  
         String info = this.codigo + " " + "codigo_evento con un get" + " " + this.totalPagar + " " +this.estadoPago + " " + this.idTransaccion + " " + this.fechaRegistro;
         ManejoArchivos.EscribirArchivo("ordenPago.txt",info);
@@ -111,7 +103,7 @@ public class OrdenPago {
     
     //Generar codigo aleatorio
     
-    private static int obtieneCodigo() {
+    private static int obtieneCodigo() { //Obtener codigo de forma aleatoria
        Random r = new Random();
        return r.nextInt(1000);
     }
